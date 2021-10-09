d4fba53a30bd1fb9882d66b17e44b7af3b56e938
Ervin Joa <erwin.joa@hotmail.de>
2021-10-09 21:03:14 +0200

    Updates to training, training data and doc
    - Updated documentation for some methods
    - Added new entries for custom training-data
    - Updated training for more epochs

--- backend/update_database.py
+++ backend/update_database.py
@@ -6,7 +6,18 @@ from urllib.error import HTTPError
 
 
 
+'''
+@author Ervin Joa
 
+This method gets the next chunk from the ClinicalTrials.gov-API
+
+@param current_border: int-value of the current pointer
+@param tmp_boarder: int-value of the current boarder + 1000 (max. allowed chunk-size)
+
+@except HTTPError in case the API blocks further downloads (possibly due to ddos-prevention)
+
+@return dataframe as a chunk
+'''
 def get_new_chunk(current_border, tmp_border):
     print("Current Border: " + current_border + ", tmp_border: " + tmp_border)
 
@@ -19,16 +30,23 @@ def get_new_chunk(current_border, tmp_border):
         print("Error, trying to continue the update")
         return get_new_chunk(current_border, tmp_border)
 
+'''
+@author Ervin Joa
+
+Downloads the new database version via the ClinicalTrials.gov-API. 
+After download completed created 2 .csv-files: 
+1) File containing the whole data
+2) File containing filtered data with only relevant entries and features
+
+@return dataframe as a chunk
+'''
 def download_data():
 
     # retrieve the information of the data avalible
-
     url_info="https://clinicaltrials.gov/api/query/study_fields?min_rnk=1&max_rnk=1&fields=NCTId&fmt=csv"
     info = pd.read_csv(url_info, nrows=8, names=["art"])
 
-
     max_border = int(info.iloc[3]["art"].split(": ")[1])
-
 
     current_border = 1
     tmp_border = 1000
@@ -68,8 +86,13 @@ def download_data():
 
     # create input_data.csv
     date_new = datetime.datetime.now().strftime("%m%d%Y")
-    input_data.to_csv("input_data_raw_" + date_new +".csv")
+
+    # Only use quarter of the database, to avoid upload errors on Github, remove in production
+    input_data.head((input_data.shape[0]//4)).to_csv("input_data_raw_" + date_new +".csv")
+
+    # filtering data, which only consits of finished trails with results
     filtered = input_data[(input_data["OverallStatus"] == "Completed") & (input_data["WhyStopped"].astype(str) == "nan")]
+
+    # droping unneeded features
     export = filtered.drop(columns=["WhyStopped", "OverallStatus"], axis = 1)
     export.to_csv("input_data_filtered.csv")
-    # input_data = input_data[0:]
--- frontend/README.md
+++ frontend/README.md
@@ -6,15 +6,15 @@
 # install dependencies
 $ npm install
 
+# update dependencies
+$ npm update
+
 # serve with hot reload at localhost:3000
 $ npm run dev
 
-# build for production and launch server
+# build for production and launch server (for hosting)
 $ npm run build
 $ npm run start
 
-# generate static project
-$ npm run generate
-```
 
 For detailed explanation on how things work, check out [Nuxt.js docs](https://nuxtjs.org).
