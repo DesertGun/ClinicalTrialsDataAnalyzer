f6ea3950d152625984f95b96d29043f7d185d909
Ervin Joa <erwin.joa@hotmail.de>
2021-10-12 20:43:11 +0200

    Bug fixes, formating and new results database

--- backend/ner_processing.py
+++ backend/ner_processing.py
@@ -1,5 +1,6 @@
 from numpy import nan
-import spacy, random
+import spacy
+import random
 from spacy.util import minibatch, compounding
 from spacy.training import Example
 import pandas as pd
@@ -15,8 +16,9 @@ database and creates the results of the tagging as a csv file.
 
 @return: DateFrame as .csv file
 '''
+
+
 def create_results():
-    
 
     # Loading the input_data
     data = pd.read_csv("input_data_filtered.csv")
@@ -24,13 +26,14 @@ def create_results():
     # Converting the features into strings
     data["PrimaryOutcomeMeasure"] = data["PrimaryOutcomeMeasure"].astype(str)
     data["PrimaryTimeFrame"] = data["PrimaryTimeFrame"].astype(str)
-    data["SecondaryOutcomeMeasure"] = data["SecondaryOutcomeMeasure"].astype(str)
+    data["SecondaryOutcomeMeasure"] = data["SecondaryOutcomeMeasure"].astype(
+        str)
     data["SecondaryTimeFrame"] = data["SecondaryTimeFrame"].astype(str)
 
     results_df = data_to_dataframe(data)
 
     results_df = results_df[(results_df["Change"] != "Not Mentioned") & (results_df["Variable"] != "Not Mentioned") &
-                        (results_df["Reference"] != "Not Mentioned") & (results_df["Timepoint"] != "Not Mentioned")]
+                            (results_df["Reference"] != "Not Mentioned") & (results_df["Timepoint"] != "Not Mentioned")]
 
     export = results_df
     export["Condition"] = export["Condition"].str.title()
@@ -38,8 +41,9 @@ def create_results():
     export["Variable"] = export["Variable"].str.title()
     export["Timepoint"] = export["Timepoint"].str.title()
     export["Change"] = export["Change"].str.title()
-
+    export.replace({'Nan': nan}, inplace=True)
     export = export.fillna('Not Mentioned')
+
     export.rename_axis("Index", axis='index', inplace=True)
     export.to_csv("results.csv")
 
@@ -55,15 +59,18 @@ filled with the new labels.
 
 @return: DateFrame
 '''
+
+
 def data_to_dataframe(data):
-    dfTest = pd.DataFrame(columns=["NCTId","Condition","Change","Reference", "Variable", "Timepoint"])
+    df = pd.DataFrame(
+        columns=["NCTId", "Condition", "Change", "Reference", "Variable", "Timepoint"])
 
     for index, row in data.iterrows():
         pointer = 0
         trial_id = row[1]
 
         condition = row[2]
-        
+
         primary_endpoint = row[3]
         primary_endpoints = []
 
@@ -77,20 +84,21 @@ def data_to_dataframe(data):
         secondary_endpoint_timepoints = []
 
         multiple_conditions = []
-        
-        if  primary_endpoint.find("|") != -1:
+
+        if primary_endpoint.find("|") != -1:
             primary_endpoints = primary_endpoint.split("|")
 
-        if  primary_endpoint_timepoint.find("|") != -1:
+        if primary_endpoint_timepoint.find("|") != -1:
             primary_endpoint_timepoints = primary_endpoint_timepoint.split("|")
 
-        if  secondary_endpoint.find("|") != -1:
+        if secondary_endpoint.find("|") != -1:
             secondary_endpoints = secondary_endpoint.split('|')
 
-        if  secondary_endpoint_timepoint.find("|") != -1:
-            secondary_endpoint_timepoints = secondary_endpoint_timepoint.split('|')
+        if secondary_endpoint_timepoint.find("|") != -1:
+            secondary_endpoint_timepoints = secondary_endpoint_timepoint.split(
+                '|')
 
-        if  condition.find("|") != -1:
+        if condition.find("|") != -1:
             multiple_conditions = condition.split('|')
 
             end_condition = ", "
@@ -98,34 +106,37 @@ def data_to_dataframe(data):
 
             for cond in multiple_conditions:
                 end_condition_items.append(cond)
-            
+
             end_condition = end_condition.join(end_condition_items)
             condition = end_condition
-            
-        
+
         if primary_endpoints == []:
             endpoint_art = "Primary Endpoint"
-            dfTemp = process_text_entry(trial_id, condition, primary_endpoint, endpoint_art, pointer, primary_endpoint_timepoint)
-            dfTest = dfTest.append(dfTemp, ignore_index= True)
+            df_temp = process_text_entry(
+                trial_id, condition, primary_endpoint, endpoint_art, pointer, primary_endpoint_timepoint)
+            df = df.append(df_temp, ignore_index=True)
         else:
             for point, timepoint in zip(primary_endpoints, primary_endpoint_timepoints):
                 endpoint_art = "Primary Endpoint"
-                dfTemp = process_text_entry(trial_id, condition, point, endpoint_art, pointer, timepoint)
-                dfTest = dfTest.append(dfTemp, ignore_index= True)
-
+                df_temp = process_text_entry(
+                    trial_id, condition, point, endpoint_art, pointer, timepoint)
+                df = df.append(df_temp, ignore_index=True)
+                pointer += 1
 
         if secondary_endpoints == []:
-            endpoint_art="Secondary Endpoint"
-            dfTemp = process_text_entry(trial_id, condition, secondary_endpoint, endpoint_art, pointer, secondary_endpoint_timepoint)
-            dfTest = dfTest.append(dfTemp, ignore_index= True)
+            endpoint_art = "Secondary Endpoint"
+            df_temp = process_text_entry(
+                trial_id, condition, secondary_endpoint, endpoint_art, pointer, secondary_endpoint_timepoint)
+            df = df.append(df_temp, ignore_index=True)
         else:
             for point, timepoint in zip(secondary_endpoints, secondary_endpoint_timepoints):
                 endpoint_art = "Secondary Endpoint"
-                dfTemp = process_text_entry(trial_id, condition, point,endpoint_art, pointer, timepoint)
-                dfTest = dfTest.append(dfTemp, ignore_index= True)
-                pointer+=1
+                df_temp = process_text_entry(
+                    trial_id, condition, point, endpoint_art, pointer, timepoint)
+                df = df.append(df_temp, ignore_index=True)
+                pointer += 1
 
-    return dfTest
+    return df
 
 
 '''
@@ -139,8 +150,10 @@ the same label, concatinates them into a single String.
 
 @return: String
 '''
+
+
 def get_labels(target_list):
-    label_list=[]
+    label_list = []
 
     for el in target_list:
         if el.label_ == "Reference" or el.label_ == "Variable":
@@ -150,6 +163,7 @@ def get_labels(target_list):
 
     label_str = ", ".join(map(str, label_list))
     return label_str
+
 
 '''
 @author Ervin Joa
@@ -167,24 +181,26 @@ features are considered.
 @param timepoint: timepoint of the trials endpoint found
 
 @return: DateFrame for the specific trial
-''' 
+'''
+
+
 def process_text_entry(nctid, condition, text, endpoint_art, pointer, timepoint):
-    dfTemp = pd.DataFrame(columns=["NCTId","Condition","Change","Reference", "Variable", "Timepoint", "Endpoint Art"])
+    df_temp = pd.DataFrame(columns=[
+                          "NCTId", "Condition", "Change", "Reference", "Variable", "Timepoint", "Endpoint Art"])
 
     target = nlp(text)
     target_list = list(target.ents)
 
-    label_str =  get_labels(target_list)
-    
+    label_str = get_labels(target_list)
 
     for entity in target.ents:
-        dfTemp.at[pointer,"NCTId"]=nctid
-        dfTemp.at[pointer,"Condition"]=condition
+        df_temp.at[pointer, "NCTId"] = nctid
+        df_temp.at[pointer, "Condition"] = condition
         if entity.label_ != "Condition":
-            dfTemp[entity.label_]=entity.text
-        dfTemp.at[pointer,"Endpoint Art"]=endpoint_art
+            df_temp[entity.label_] = entity.text
+        df_temp.at[pointer, "Endpoint Art"] = endpoint_art
         if entity.label_ == "Reference" or entity.label_ == "Variable":
-             dfTemp[entity.label_]= label_str
+            df_temp[entity.label_] = label_str
         if entity.label_ != "Timepoint":
-            dfTemp.at[pointer, "Timepoint"] = timepoint
-    return dfTemp
+            df_temp.at[pointer, "Timepoint"] = timepoint
+    return df_temp
--- backend/update_database.py
+++ backend/update_database.py
@@ -5,7 +5,6 @@ import datetime
 from urllib.error import HTTPError
 
 
-
 '''
 @author Ervin Joa
 
@@ -18,17 +17,22 @@ This method gets the next chunk from the ClinicalTrials.gov-API
 
 @return dataframe as a chunk
 '''
+
+
 def get_new_chunk(current_border, tmp_border):
     print("Current Border: " + current_border + ", tmp_border: " + tmp_border)
 
-    url_new_chunk="https://clinicaltrials.gov/api/query/study_fields?min_rnk=" + current_border +"&max_rnk="+tmp_border+"&fields=NCTId,Condition,OverallStatus,PrimaryOutcomeMeasure,PrimaryOutcomeTimeFrame,SecondaryOutcomeMeasure,SecondaryOutcomeTimeFrame,WhyStopped,BaselineMeasureTitle&fmt=csv"
+    url_new_chunk = "https://clinicaltrials.gov/api/query/study_fields?min_rnk=" + current_border + "&max_rnk="+tmp_border + \
+        "&fields=NCTId,Condition,OverallStatus,PrimaryOutcomeMeasure,PrimaryOutcomeTimeFrame,SecondaryOutcomeMeasure,SecondaryOutcomeTimeFrame,WhyStopped,BaselineMeasureTitle&fmt=csv"
     try:
-        data_new_chunk = pd.read_csv(url_new_chunk,  skiprows = 11, names=["NCTId","Condition","OverallStatus","PrimaryOutcomeMeasure","PrimaryTimeFrame","SecondaryOutcomeMeasure","SecondaryTimeFrame", "WhyStopped", "BaselineMeasureTitle"])
+        data_new_chunk = pd.read_csv(url_new_chunk,  skiprows=11, names=[
+                                     "NCTId", "Condition", "OverallStatus", "PrimaryOutcomeMeasure", "PrimaryTimeFrame", "SecondaryOutcomeMeasure", "SecondaryTimeFrame", "WhyStopped", "BaselineMeasureTitle"])
         return data_new_chunk
     except HTTPError:
         time.sleep(5)
         print("Error, trying to continue the update")
         return get_new_chunk(current_border, tmp_border)
+
 
 '''
 @author Ervin Joa
@@ -40,19 +44,20 @@ After download completed created 2 .csv-files:
 
 @return dataframe as a chunk
 '''
+
+
 def download_data():
 
     # retrieve the information of the data avalible
-    url_info="https://clinicaltrials.gov/api/query/study_fields?min_rnk=1&max_rnk=1&fields=NCTId&fmt=csv"
+    url_info = "https://clinicaltrials.gov/api/query/study_fields?min_rnk=1&max_rnk=1&fields=NCTId&fmt=csv"
     info = pd.read_csv(url_info, nrows=8, names=["art"])
 
     max_border = int(info.iloc[3]["art"].split(": ")[1])
 
     current_border = 1
     tmp_border = 1000
-    input_data = pd.DataFrame(columns=["NCTId","Condition","OverallStatus","PrimaryOutcomeMeasure","PrimaryTimeFrame",
-                                "SecondaryOutcomeMeasure","SecondaryTimeFrame", "WhyStopped"])
-
+    input_data = pd.DataFrame(columns=["NCTId", "Condition", "OverallStatus", "PrimaryOutcomeMeasure", "PrimaryTimeFrame",
+                                       "SecondaryOutcomeMeasure", "SecondaryTimeFrame", "WhyStopped"])
 
     # Phase 1: download the data and create a input_data.csv
     # get and concat the data, while the max_border not reached
@@ -60,10 +65,11 @@ def download_data():
     while tmp_border < max_border:
 
         # gets the new chunk from min_rnk:max_rnk
-        input_data_current_chunk = get_new_chunk(str(current_border), str(tmp_border))
+        input_data_current_chunk = get_new_chunk(
+            str(current_border), str(tmp_border))
 
         # concat old state with new data_chunk
-        concat_frames = [input_data, input_data_current_chunk] 
+        concat_frames = [input_data, input_data_current_chunk]
 
         new_data_merge = pd.concat(concat_frames)
         print("rows in Result_DF: " + str(len(new_data_merge)))
@@ -76,23 +82,25 @@ def download_data():
         # wait 5 seconds to avoid spam-protection
         time.sleep(5)
 
-
     # the last < 1000 values will be retrieved by using new borders based on the diff.
     diff_border = max_border - current_border
-    input_data_last_chunk = get_new_chunk(str(current_border), str(current_border + diff_border))
+    input_data_last_chunk = get_new_chunk(
+        str(current_border), str(current_border + diff_border))
 
-    concat_frames_final = [input_data, input_data_last_chunk] 
+    concat_frames_final = [input_data, input_data_last_chunk]
     input_data = pd.concat(concat_frames_final)
 
     # create input_data.csv
     date_new = datetime.datetime.now().strftime("%m%d%Y")
 
     # Only use quarter of the database, to avoid upload errors on Github, remove in production
-    input_data.head((input_data.shape[0]//4)).to_csv("input_data_raw_" + date_new +".csv")
+    input_data.head(
+        (input_data.shape[0]//4)).to_csv("input_data_raw_" + date_new + ".csv")
 
     # filtering data, which only consits of finished trails with results
-    filtered = input_data[(input_data["OverallStatus"] == "Completed") & (input_data["WhyStopped"].astype(str) == "nan")]
+    filtered = input_data[(input_data["OverallStatus"] == "Completed") & (
+        input_data["WhyStopped"].astype(str) == "nan")]
 
     # droping unneeded features
-    export = filtered.drop(columns=["WhyStopped", "OverallStatus"], axis = 1)
+    export = filtered.drop(columns=["WhyStopped", "OverallStatus"], axis=1)
     export.to_csv("input_data_filtered.csv")
